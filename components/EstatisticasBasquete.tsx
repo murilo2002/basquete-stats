a8ed88364aefdbad4c2910f90f983b9b0181b667
murilo2002 <murilo.alm.cunha@gmail.com>
2025-05-16 11:59:25 -0300

    Update EstatisticasBasquete.tsx

--- components/EstatisticasBasquete.tsx
+++ components/EstatisticasBasquete.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 export default function EstatisticasBasquete() {
   const [jogadores, setJogadores] = useState([
