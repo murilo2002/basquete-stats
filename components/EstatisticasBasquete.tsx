84353befbeb2fabdfbf42fd0632801a4e708c125
murilo2002 <murilo.alm.cunha@gmail.com>
2025-05-16 15:08:21 -0300

    Update EstatisticasBasquete.tsx

--- components/EstatisticasBasquete.tsx
+++ components/EstatisticasBasquete.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState, ChangeEvent } from "react";
-import { Card, CardContent } from "../ui/card";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 type Jogador = {
   id: number;
