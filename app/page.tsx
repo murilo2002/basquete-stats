e0d09f9cbd219f1db612c4cca5768df960d592d1
murilo2002 <murilo.alm.cunha@gmail.com>
2025-05-16 11:18:35 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  );
-}
-
 import EstatisticasBasquete from "@/components/EstatisticasBasquete";
 
 export default function Home() {
