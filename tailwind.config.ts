12784c5730b0134c223a257c977e7f15f1af2d5e
murilo2002 <murilo.alm.cunha@gmail.com>
2025-05-16 14:45:23 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,14 @@
-export default {}
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",      // inclui pastas e arquivos do app
+    "./components/**/*.{ts,tsx,js,jsx}" // inclui componentes
+  ],
+  theme: {
+    extend: {}, // caso queira personalizar depois
+  },
+  plugins: [],
+};
+
+export default config;
