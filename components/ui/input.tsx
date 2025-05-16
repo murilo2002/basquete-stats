9825b857be6af981b234d0c63f09886d4b1c1194
murilo2002 <murilo.alm.cunha@gmail.com>
2025-05-16 15:00:58 -0300

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 export function Input(props: InputProps) {
-  return <input {...props} />;
+  return (
+    <input
+      {...props}
+      className={`border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${props.className ?? ""}`}
+    />
+  );
 }
